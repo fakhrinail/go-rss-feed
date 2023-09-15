71941d80473cc745e1603611077cd63d8967b144
fakhrinailwbw <fakhrinailwbw@gmail.com>
2023-09-15 15:01:52 +0700

    feat: add feeds

--- main.go
+++ main.go
@@ -58,6 +58,8 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Post("/user", apiCfg.handlerCreateUser)
 	v1Router.Get("/user", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
 	router.Mount("/v1", v1Router)
 
